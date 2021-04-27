4d7200d39df653da70e0935d1cd8d47105905fc6
César Guadarrama <cesargdm@icloud.com>
2021-04-26 22:15:10 -0500

    Add config

---
+++ index.js
@@ -0,0 +1,69 @@
+const MAX_PARAMS = 3
+
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'jsx-a11y', 'import', 'react-hooks', 'react'],
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+
+    'prettier',
+
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/strict',
+
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  ignorePatterns: 'src/generated/graphql.tsx',
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': 'error',
+
+    'max-lines': ['warn', { max: 500, skipBlankLines: true }],
+    'max-params': ['warn', MAX_PARAMS],
+    'no-console': 'warn',
+    'no-magic-numbers': ['warn', { ignoreArrayIndexes: true }],
+    'no-unused-expressions': 'error',
+    'no-useless-return': 'error',
+    'prefer-const': 'error',
+    'prefer-destructuring': 'warn',
+    'prefer-spread': 'warn',
+    'require-await': 'error',
+    'no-unused-vars': 'off',
+
+    'react/react-in-jsx-scope': 'off',
+
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+        ],
+        'newlines-between': 'always-and-inside-groups',
+      },
+    ],
+    'import/exports-last': 'warn',
+  },
+  settings: {
+    react: { version: 'detect' },
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
+}
