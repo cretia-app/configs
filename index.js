a75eb5f0f1da44ee4b2820aed72f3678ab25bf0c
César Guadarrama <cesargdm@icloud.com>
2022-11-03 14:46:00 -0600

    Update rules

--- index.js
+++ index.js
@@ -1,80 +1,84 @@
 const MAX_PARAMS = 3
 
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint', 'jsx-a11y', 'import', 'react-hooks', 'react'],
-  env: {
-    es6: true,
-    node: true,
-    jest: true,
-    browser: true,
-  },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
+	root: true,
+	parser: '@typescript-eslint/parser',
+	plugins: ['@typescript-eslint', 'jsx-a11y', 'import', 'react-hooks', 'react'],
+	env: {
+		es6: true,
+		node: true,
+		jest: true,
+		browser: true,
+	},
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
 
-    'prettier',
+		'prettier',
 
-    'plugin:react/recommended',
+		'plugin:react/recommended',
 
-    'plugin:react-hooks/recommended',
+		'plugin:react-hooks/recommended',
 
-    'plugin:jsx-a11y/strict',
+		'plugin:jsx-a11y/strict',
 
-    'plugin:import/errors',
-    'plugin:import/warnings',
-    'plugin:import/typescript',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
 
-    'plugin:compat/recommended',
-  ],
-  ignorePatterns: 'src/generated/graphql.tsx',
-  rules: {
-    'max-lines': ['warn', { max: 500, skipBlankLines: true }],
-    'max-params': ['warn', MAX_PARAMS],
-    'no-console': 'warn',
+		'plugin:compat/recommended',
+	],
+	ignorePatterns: 'src/generated/graphql.tsx',
+	rules: {
+		'max-lines': ['warn', { max: 500, skipBlankLines: true }],
+		'max-params': ['warn', MAX_PARAMS],
+		'no-console': 'warn',
 
-    'no-magic-numbers': [
-      'warn',
-      { ignoreArrayIndexes: true, ignore: [-1, 0, 1] },
-    ],
+		'no-magic-numbers': [
+			'warn',
+			{ ignoreArrayIndexes: true, ignore: [-1, 0, 1] },
+		],
 
-    'no-unused-expressions': 'warn',
-    'no-useless-return': 'error',
-    'prefer-const': 'error',
-    'prefer-destructuring': 'warn',
-    'prefer-spread': 'warn',
-    'require-await': 'error',
-    'no-unused-vars': 'off',
+		'no-unused-expressions': 'warn',
+		'no-useless-return': 'error',
+		'prefer-const': 'error',
+		'prefer-destructuring': 'warn',
+		'prefer-spread': 'warn',
+		'require-await': 'error',
+		'no-unused-vars': 'off',
 
-    '@typescript-eslint/no-explicit-any': 'off',
-    '@typescript-eslint/no-unused-vars': 'warn',
+		// TypeScript
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unused-vars': 'warn',
 
-    'react/react-in-jsx-scope': 'off',
+		// React
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-no-leaked-render': 'warning',
 
-    'import/order': [
-      'warn',
-      {
-        groups: [
-          'builtin',
-          'external',
-          'internal',
-          'parent',
-          'sibling',
-          'index',
-        ],
-        'newlines-between': 'always-and-inside-groups',
-      },
-    ],
-    'import/exports-last': 'warn',
-  },
-  settings: {
-    react: { version: 'detect' },
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        moduleDirectory: ['node_modules', 'src'],
-      },
-    },
-  },
+		// Import order
+		'import/order': [
+			'warn',
+			{
+				groups: [
+					'builtin',
+					'external',
+					'internal',
+					'parent',
+					'sibling',
+					'index',
+				],
+				'newlines-between': 'always-and-inside-groups',
+			},
+		],
+		'import/exports-last': 'warn',
+	},
+	settings: {
+		react: { version: 'detect' },
+		'import/resolver': {
+			node: {
+				extensions: ['.js', '.jsx', '.ts', '.tsx'],
+				moduleDirectory: ['node_modules', 'src'],
+			},
+		},
+	},
 }
